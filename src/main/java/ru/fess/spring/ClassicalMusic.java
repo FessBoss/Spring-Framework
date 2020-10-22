c0538f162bf9ba54e49412b4a59f6bc0fa6d9016
Evgeniy Mereha <skyflex713@gmail.com>
2020-10-22 13:29:32 +0600

    Добавили простейшую реализацию Инверсии управления (IoC) с помощью Spring Framework.

---
+++ src/main/java/ru/fess/spring/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package ru.fess.spring;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Классическая музыка";
+    }
+}
---
+++ src/main/java/ru/fess/spring/Music.java
@@ -0,0 +1,5 @@
+package ru.fess.spring;
+
+public interface Music {
+    String getSong();
+}
