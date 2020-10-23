ce0c7e4b7be8f46829e96e066d434502b7547086
Evgeniy Mereha <skyflex713@gmail.com>
2020-10-23 12:12:35 +0600

    Добавили init/destroy - method для бинов

--- src/main/java/ru/fess/spring/ClassicalMusic.java
+++ src/main/java/ru/fess/spring/ClassicalMusic.java
@@ -1,6 +1,15 @@
 package ru.fess.spring;
 
 public class ClassicalMusic implements Music {
+
+    public void doMyInit() {
+        System.out.println("Происходит инициализация бина ClassicalMusic");
+    }
+
+    public void doMuDestroy() {
+        System.out.println("Происходит уничтожение бина ClassicalMusic");
+    }
+
     @Override
     public String getSong() {
         return "Классическая музыка";
