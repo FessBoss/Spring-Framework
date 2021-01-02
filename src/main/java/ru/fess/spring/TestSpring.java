1576141fc2178197f6534e7f1f4e7384320126d2
Evgeniy Mereha <skyflex713@gmail.com>
2021-01-02 07:41:09 +0600

    Аннотация @Autowired. Внедрение зависимостей.

--- src/main/java/ru/fess/spring/TestSpring.java
+++ src/main/java/ru/fess/spring/TestSpring.java
@@ -8,14 +8,20 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("rockMusic", Music.class);
-        Music music2 = context.getBean("classicalMusic", Music.class);
+//        Music music = context.getBean("rockMusic", Music.class);
+//        Music music2 = context.getBean("classicalMusic", Music.class);
+//
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
+//
+//        musicPlayer.playMusic();
+//        musicPlayer2.playMusic();
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
 
-        musicPlayer.playMusic();
-        musicPlayer2.playMusic();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
