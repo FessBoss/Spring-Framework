06df05e1841300d3e3ba046cae5e5991c582a5a6
Evgeny Mereha <skyflex713@gmail.com>
2021-02-23 15:50:48 +0700

    Конфигурация с помощью Java кода

--- src/main/java/ru/fess/spring/TestSpring.java
+++ src/main/java/ru/fess/spring/TestSpring.java
@@ -1,24 +1,13 @@
 package ru.fess.spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
         );
-
-//        Music music = context.getBean("rockMusic", Music.class);
-//        Music music2 = context.getBean("classicalMusic", Music.class);
-//
-//        MusicPlayer musicPlayer = new MusicPlayer(music);
-//        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
-//
-//        musicPlayer.playMusic();
-//        musicPlayer2.playMusic();
-
-//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        musicPlayer.playMusic();
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
